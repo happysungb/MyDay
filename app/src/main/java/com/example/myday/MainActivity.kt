f797af5a6e1c9d5df4e780d6604298cb62c0734c
making-a-scene <4936joo@naver.com>
2023-12-02 18:30:26 +0900

    navigation view 설정, 칼로리 계산 페이지 수정

--- app/src/main/java/com/example/myday/MainActivity.kt
+++ app/src/main/java/com/example/myday/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.myday
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.myday.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val drawerLayout = binding.drawer
+        binding.buttonOpenNav.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        val foodEditText = binding.foodEt
+        val kcalSearchBtn = binding.kcalSearchBtn
+        binding.kcalSearchBtn.setOnClickListener {
+            val inputText = foodEditText.text.toString()
+
+            val url = "http://openapi.foodsafetykorea.go.kr/api/27dfc35a066042d49e98/I2790/json/1/5/DESC_KOR=${inputText}"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+
+
+
+
     }
+
 }
