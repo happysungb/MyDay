4eab157be342503d0bf6264ef84a9364d1438aed
making-a-scene <4936joo@naver.com>
2023-12-10 17:35:37 +0900

    음식 선택 완료 -> 아침, 점심, 저녁, 간식, 야식 중에 하나 선택 -> 다음 버튼 클릭시 다음 페이지로 이동하도록 함

--- app/src/main/java/com/example/myday/MainActivity.kt
+++ app/src/main/java/com/example/myday/MainActivity.kt
@@ -5,15 +5,17 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myday.databinding.ActivityMainBinding
+import com.example.myday.food.CalcKcalActivity
 import com.example.myday.food.DialogCallback
 import com.example.myday.food.FoodNutrition
 import com.example.myday.food.GetKcalInfo
@@ -21,6 +23,7 @@ import com.example.myday.food.Row
 import com.example.myday.food.SearchResultFragment
 import com.example.myday.food.Selected
 import com.example.myday.food.SelectedAdapter
+import com.example.myday.food.Time
 import com.google.android.material.navigation.NavigationView
 import retrofit2.Call
 import retrofit2.Callback
@@ -52,6 +55,18 @@ class MainActivity : AppCompatActivity(), DialogCallback, NavigationView.OnNavig
 
         // NavigationView 리스너 설정
         mainBinding.navView.setNavigationItemSelectedListener(this)
+
+        // Spinner 설정(아침, 점심, 저녁, 야식, 간식 중 선택)
+        val spinner: Spinner = mainBinding.spinner
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.`when`,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.setSelection(0) // 초기값은 아침으로 설정
 
         // 칼로리 검색 버튼 클릭시
         val foodEditText = mainBinding.foodEt
@@ -95,8 +110,24 @@ class MainActivity : AppCompatActivity(), DialogCallback, NavigationView.OnNavig
             })
 
         }
+
+        // 하단에 있는 다음 버튼 클릭시
         mainBinding.nextBtn.setOnClickListener {
-            //
+            if (selected.isEmpty()) {
+                Toast.makeText(this, "선택된 음식이 아직 없어요!", Toast.LENGTH_SHORT).show()
+            } else {
+                val time: Time = when (spinner.selectedItem.toString()) {
+                    "아침" -> Time.BREAKFAST
+                    "점심" -> Time.LUNCH
+                    "저녁" -> Time.DINNER
+                    "야식" -> Time.SUPPER
+                    else -> Time.SNACK
+                }
+                val intent = Intent(this, CalcKcalActivity::class.java)
+                intent.putExtra("selectedList", ArrayList(selected))
+//                intent.putExtra("time", time)
+//                startActivity(intent)
+            }
         }
 
     }
@@ -133,8 +164,9 @@ class MainActivity : AppCompatActivity(), DialogCallback, NavigationView.OnNavig
                 add<SearchResultFragment>(R.id.fragment_container, args = bundle)
             }
         }
-
     }
+
+
 
 
 }
