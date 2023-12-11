52c5817f01304d31c0ef3fad065602f4749a2dd6
happysungb <darkisunho@naver.com>
2023-12-12 02:04:54 +0900

    splash screen -> 로그인 화면 이동하게끔 구현 로그인 화면에서 My Day 계정 만들기 버튼 누르면 회원가입 화면 연결

--- app/src/main/java/com/example/myday/IntroActivity.java
+++ app/src/main/java/com/example/myday/IntroActivity.java
@@ -4,24 +4,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import com.example.myday.user.LoginActivity;
 
-public class IntroActivity extends Activity{
-    protected void onCreate(Bundle savedInstanceState){
+public class IntroActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_layout);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                // LoginActivity로 인텐트 변경
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
         }, 1500);
     }
 
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         finish();
     }
 }
+
--- app/src/main/java/com/example/myday/user/LoginActivity.kt
+++ app/src/main/java/com/example/myday/user/LoginActivity.kt
@@ -1,7 +1,22 @@
 package com.example.myday.user
 
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myday.R
 
 class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
 
+        val joinButton: Button = findViewById(R.id.join_btn)
+        joinButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
