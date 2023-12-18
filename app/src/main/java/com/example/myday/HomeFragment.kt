958226d4bec834c3c4e5422ee4c067f50f02cc2f
happysungb <darkisunho@naver.com>
2023-12-19 01:19:39 +0900

    홈화면에서 권장칼로리 계산해서 띄우기 성공

--- app/src/main/java/com/example/myday/HomeFragment.kt
+++ app/src/main/java/com/example/myday/HomeFragment.kt
@@ -1,16 +1,21 @@
 package com.example.myday
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myday.databinding.FragmentHomeBinding
+import com.example.myday.user.Gender
+import com.example.myday.user.UserDto
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.Calendar
+import java.util.Date
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -32,7 +37,6 @@ class HomeFragment : Fragment() {
         auth = FirebaseAuth.getInstance()
         currentUser = auth.currentUser
 
-        // Firebase Authentication에서 현재 사용자 가져오기
         val user = FirebaseAuth.getInstance().currentUser
 
         // 현재 사용자가 있을 경우 Cloud Firestore에서 사용자 정보 가져오기
@@ -43,21 +47,102 @@ class HomeFragment : Fragment() {
                 .addOnSuccessListener { document ->
                     if (document.exists()) {
                         val userName = document.getString("name") ?: "사용자"
+                        val userDto = document.toObject(UserDto::class.java) // UserDto로 변환
+
+                        // UserDto에서 성별 정보 가져오기
+                        val userGender = userDto?.gender
+
+                        // UserDto에서 생년월일, 체중, 신장 정보 가져오기
+                        val userBirthDate = userDto?.birthDate
+                        val userWeight = userDto?.weight?.toInt() ?: 0
+                        val userHeight = userDto?.height?.toInt() ?: 0
+
+                        // 로그로 변수 값 확인
+                        Log.d("HomeFragment", "User Gender: $userGender")
+                        Log.d("HomeFragment", "User Weight: $userWeight")
+                        Log.d("HomeFragment", "User Height: $userHeight")
+
+                        // 생년월일 정보로 나이 계산
+                        val age = calculateAge(userBirthDate)
+
+                        // 권장 칼로리 계산
+                        val recommendedCalories = calculateRecommendedCalories(userGender, age, userWeight, userHeight)
+
+                        // 텍스트뷰에 권장 칼로리 표시
+                        binding.homeKcal.text = "500/${recommendedCalories}kcal"
+
+                        binding.homeRecommendedIntake.text = "목표 칼로리: $recommendedCalories kcal"
+
                         binding.greetingTextView.text = "${userName}님, 안녕하세요."
                     } else {
                         // 문서가 없을 경우 처리
                         binding.greetingTextView.text = "사용자 정보 없음"
+                        binding.homeRecommendedIntake.text = "목표 칼로리: 정보 없음" // 혹은 다른 기본값 설정
                     }
                 }
                 .addOnFailureListener { e ->
                     // 오류 처리
                     binding.greetingTextView.text = "사용자 정보를 가져오지 못했습니다: $e"
+                    binding.homeRecommendedIntake.text = "목표 칼로리: 오류" // 오류 발생 시에도 처리
                 }
         } ?: run {
             // 사용자가 로그인되어 있지 않을 경우 처리
             binding.greetingTextView.text = "로그인되지 않았습니다."
+            binding.homeRecommendedIntake.text = "목표 칼로리: 로그인 필요" // 혹은 다른 기본값 설정
         }
     }
 
-    // onDestroyView() 함수는 필요 없으므로 주석 처리 또는 삭제
+    // 생년월일을 나이로 변환하는 함수
+    private fun calculateAge(birthDate: Long?): Int {
+        birthDate?.let {
+            val currentDate = Calendar.getInstance().timeInMillis
+            val ageInMillis = currentDate - birthDate
+            val ageCalendar = Calendar.getInstance()
+            ageCalendar.timeInMillis = ageInMillis
+            val age = ageCalendar.get(Calendar.YEAR) - 1970 // 만 나이로 계산
+            Log.d("HomeFragment", "Calculated Age: $age") // 계산된 나이를 로그로 출력
+            return age
+        }
+        return 0 // 생년월일이 없을 경우 0 반환
+    }
+
+    // 남성의 권장 칼로리를 계산하는 함수
+    private fun calculateRecommendedCaloriesForMale(
+        age: Int,
+        weight: Int,
+        height: Int
+    ): Int {
+        val result = (10 * weight) + (6.25 * height.toDouble()) + (5 * age) + 5
+        Log.d("CaloriesCalculation", "Male Calories: $result")
+        return result.toInt()
+    }
+
+    // 여성의 권장 칼로리를 계산하는 함수
+    private fun calculateRecommendedCaloriesForFemale(
+        age: Int,
+        weight: Int,
+        height: Int
+    ): Int {
+        val result = (10 * weight) + (6.25 * height.toDouble()) + (5 * age) - 161
+        Log.d("CaloriesCalculation", "Female Calories: $result")
+        return result.toInt()
+    }
+
+    // 사용자의 성별, 생년월일, 체중, 신장 정보를 사용하여 권장 칼로리를 계산하는 함수
+    private fun calculateRecommendedCalories(
+        gender: Gender?,
+        age: Int,
+        weight: Int,
+        height: Int
+    ): Int {
+        return when (gender) {
+            Gender.MALE -> calculateRecommendedCaloriesForMale(age, weight, height)
+            Gender.FEMALE -> calculateRecommendedCaloriesForFemale(age, weight, height)
+            else -> {
+                // 성별 정보가 없거나 unknown인 경우 기본값 또는 예외 처리
+                // 여기서 기본값 또는 예외 처리를 구현하세요.
+                0 // 임시로 0을 반환하도록 수정
+            }
+        }
+    }
 }
